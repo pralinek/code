bfb8e9ca0d1f48a85865b730746e39c3b8b9065a
Tomasz Olech <tolech89@gmail.com>
2024-05-09 09:08:15 +0200

    code

--- groupTemplate.tsx
+++ groupTemplate.tsx
@@ -8,3 +8,15 @@ groupTemplate: function(group) {
     });
     return container;
   },
+
+  <div className="group-item flex justify-between items-center bg-gray-100 p-2 rounded-lg mb-2">
+            <div className="flex items-center">
+                <FontAwesomeIcon icon={faCheck} className="text-green-500 mr-2" />
+                <div className="text-gray-800">{groupName}</div>
+            </div>
+            <div className="flex items-center">
+                <button onClick={onRemove} className="text-red-500 hover:text-red-700 focus:outline-none">
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+            </div>
+        </div>
