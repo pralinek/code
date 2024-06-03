a512f3878edf9750f20922a95bc5b6f57da385b1
Tomasz Olech <tolech89@gmail.com>
2024-06-03 19:01:31 +0200

    style

---
+++ vis-item-content.tsx
@@ -0,0 +1,9 @@
+.vis-item-content {
+    width: 100%;       /* Ensures the content fills the full width of the item */
+    height: 100%;      /* Ensures the content fills the full height of the item */
+    white-space: nowrap; /* Prevents text from wrapping, optional based on your needs */
+    overflow: hidden;  /* Hides any overflow, optional based on your needs */
+    display: flex;     /* Flexbox to help center content, if needed */
+    justify-content: center; /* Horizontally center content, if desired */
+    align-items: center; /* Vertically center content, if desired */
+}
