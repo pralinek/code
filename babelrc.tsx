e9f2efde57b84a6f08be640be544e1596067fff1
Tomasz Olech <tolech89@gmail.com>
2024-06-21 13:08:25 +0200

    some

--- babelrc.tsx
+++ babelrc.tsx
@@ -4,3 +4,22 @@
       ["@emotion/babel-plugin", { "sourceMap": true, "autoLabel": "dev-only" }]
     ]
   }
+
+
+  env: {
+    development: {
+      presets: [
+        "next/babel",
+        [
+          require.resolve('@emotion/babel-preset-css-prop'),
+          {
+            sourceMap: true,
+            autoLabel: true,
+          },
+        ],
+      ],
+    },
+    production: {
+      presets: ["next/babel", [require.resolve('@emotion/babel-preset-css-prop'), { hoist: true }]],
+    },
+  },
