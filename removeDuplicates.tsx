a38c993a71a8f62cd7d2bbdaa79483326a45c15c
Tomasz Olech <tolech89@gmail.com>
2024-07-18 09:23:15 +0200

    m

--- removeDuplicates.tsx
+++ removeDuplicates.tsx
@@ -14,3 +14,23 @@ const removeDuplicatesAndCount = (arr) => {
     // Step 2: Convert the map values to an array
     return Array.from(countMap.values());
   };
+
+  function groupEventsByDay(events) {
+    return events.reduce((groups, event) => {
+      // Parse the UTC date
+      const date = new Date(event.startTime);
+  
+      // Get the date string (e.g., '2024-07-18')
+      const dateString = date.toISOString().split('T')[0];
+  
+      // If the group for this date does not exist, create it
+      if (!groups[dateString]) {
+        groups[dateString] = [];
+      }
+  
+      // Add the event to the group
+      groups[dateString].push(event);
+  
+      return groups;
+    }, {});
+  }
