92b3a657f441a3018907d448d085061bc8d35851
Tomasz Olech <tolech89@gmail.com>
2025-02-24 12:13:44 +0100

    env

--- env.tsx
+++ env.tsx
@@ -1,23 +1,14 @@
-declare global {
-  interface Window {
-    runtime_config?: Record<string, string>;
-  }
+var config = app.Configuration.AsEnumerable();
+
+// Create a .env file content
+var envFile = new StringBuilder();
+foreach (var kvp in config)
+{
+    if (!string.IsNullOrEmpty(kvp.Value)) // Ignore null values
+    {
+        envFile.AppendLine($"REACT_APP_{kvp.Key.Replace(":", "_").ToUpper()}={kvp.Value}");
+    }
 }
 
-/**
- * Extracts all variables from process.env and merges them with window.runtime_config
- */
-export const getConfig = (): Record<string, string> => {
-  // Extract all process.env variables
-  const envVariables = Object.keys(process.env)
-    .reduce((acc, key) => {
-      acc[key] = process.env[key] || "";
-      return acc;
-    }, {} as Record<string, string>);
-
-  // Merge with window.runtime_config (which is injected at runtime)
-  return {
-    ...envVariables,
-    ...window.runtime_config,
-  };
-};
+// Write to ClientApp/.env before building React
+File.WriteAllText("ClientApp/.env", envFile.ToString());
